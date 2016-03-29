a23b329d7ac2b35269af4f04ff3fad9cc11d1140
Timo <timo.homburg@gmx.de>
2016-03-29 21:19:42 +0200

    Python 3

--- langerjan.py
+++ langerjan.py
@@ -1,12 +1,6 @@
-"""
-cleverbot.py - Costum commands for sopel bot
-Credits to LangerJan for the idea
-Copyright 2015, Timo Homburg
-Licensed under GPL3.
-"""
 import sopel.module
 import time
- 
+
 @sopel.module.commands('dance')
 def helloworld(bot, trigger):
     bot.say('<(^.^<)')
