87109513381e51d52cbd67b79af6d346e2d8847b
Timo <timo.homburg@gmx.de>
2016-04-03 00:43:35 +0200

    Update sghaze.py

--- sghaze.py
+++ sghaze.py
@@ -1,4 +1,6 @@
 from sopel.module import commands, example, NOLIMIT
+import datetime
+import iso8601
 import urllib.request, urllib.parse, urllib.error
 import json
 import re
@@ -22,7 +24,7 @@ def sghaze(bot,trigger):
                 chosen_area=chosen_area.capitalize()
                 bot.say("Air Quality Index for Singapore "+chosen_area+" district: ")
 #               bot.say(str(result))
-                bot.say(chosen_area+": "+rate_result(result[chosen_area]))
+                bot.say(rate_result(result[chosen_area]))
         else:
                 bot.say("Air Quality Index for Singapore:")
                 for key in result:
@@ -38,6 +40,38 @@ def sghazelegend(bot,trigger):
         bot.say('301-400: Hazardous')
         bot.say('>400 Beyond Hazardous')
         bot.say('More information: https://en.wikipedia.org/wiki/Air_quality_index')
+
+@commands('hazefc\s+(.+)')
+def findhazecity(bot,trigger):
+        city=trigger.group(2)
+        queryurl="http://sg1.aqicn.org/services/search/?lang=en&key=_1ca%2BQ%1Ff%0D%5D%1F%0D%2B%40H%1D%1C%1B&jsoncallback=waqiloader.jsonp.LJdMztTHefKXTry&s="+city+"&xtra&qid=2"
+        queryurl2="http://aqicn.org/services/forecast/?city="+city+"&lang=en"
+        print(queryurl2)
+        result=urllib.request.urlopen(queryurl).read().decode()
+        testdata=json.loads(result[result.index('(')+1:-2])['data'][1]
+        testurl=json.loads(result[result.index('(')+1:-2])['data'][1]['url']
+        if "faq" in testurl:
+                testurl=json.loads(result[result.index('(')+1:-2])['data'][2]['url']
+        result2=json.loads(urllib.request.urlopen(queryurl2).read().decode())
+        aqiforecast=result2['forecast']['aqi']
+        displayres={}
+        currentday=None
+        for elem in aqiforecast:
+                time=iso8601.parse_date(elem['t'])
+        if currentday==None:
+                currentday=time
+        if currentday!=time.day:
+                currentday=time.day
+                if currentday not in displayres:
+                        displayres[currentday]=[]
+                displayres[currentday].append(elem['v'][0])
+                displayres[currentday].append(elem['v'][1])
+                print(displayres)
+        bot.say("Air Quality Forecast for "+city+":")
+        for elem in displayres:
+                bot.say(str(elem)+": "+rate_result(min(displayres[elem]))+"/"+rate_result(max(displayres[elem])))
+#testurl=firstres['url']
+        #print(testurl)
 
 def rate_result(resnumber):
         if resnumber<=50:
@@ -81,3 +115,4 @@ def get_psi():
                         if m:
                                 psi[area]=int(m.group(1))
         return psi
+
