0fa424b74e3fc135f470c39d85c2ff3b72d5e72b
Timo <timo.homburg@gmx.de>
2016-04-07 23:17:53 +0200

    Added wikiquote support

--- randomquote.py
+++ randomquote.py
@@ -7,6 +7,8 @@ import urllib.request, urllib.error, urllib.parse
 import json
 import re, cgi
 import html.parser
+import wikiquote
+import random
 
 tag_re=re.compile(r'(<!--.*?-->|<[^>]*>)')
 
@@ -18,3 +20,14 @@ def randquote(bot,trigger):
     data = json.loads(result.decode())
     bot.say("\""+html.unescape(tag_re.sub('',data[0]['content']))+"\"")
     bot.say(html.unescape(data[0]['title']))#+" - "+data[0]['link'])
+
+@commands('quoteofday','qod')
+@example('.quoteofday')
+def quoteofday(bot,trigger):
+    quoteofday=wikiquote.quote_of_the_day()
+    bot.say(quoteofday[0])
+    bot.say(quoteofday[1])    
+
+@commands('quote\s([A-z\s]+)')
+def quoteofperson(bot,trigger):
+    bot.say(str(random.choice(wikiquote.quotes(trigger.
