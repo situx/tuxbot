e8481064b56cc624bdcc865ada93a816cd760188
Timo <timo.homburg@gmx.de>
2016-04-11 23:26:12 +0200

    Create kquote.py

---
+++ kquote.py
@@ -0,0 +1,17 @@
+import xml.etree.ElementTree as ET
+import sopel.module
+import random
+
+quotehash={}
+
+def setup(bot):
+    e = ET.parse('/home/bananapi/.sopel/modules/kquote.xml').getroot()
+    for atype in e.findall('quote'):
+        quotehash[atype.get('author')]=atype.text
+
+
+@sopel.module.commands('kquote')
+def wortspielcredit(bot,trigger):
+    choose=random.choice(list(quotehash.keys()))
+    bot.say(quotehash[choose])
+    bot.say(choose)
