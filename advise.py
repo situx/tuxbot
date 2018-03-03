babc7f92b21ee4eb68506188c3039a0a1e5ae814
situx <timo.bomburg@gmx.de>
2018-03-03 12:11:10 +0100

    Invest and Advise Module Changes

--- advise.py
+++ advise.py
@@ -9,17 +9,19 @@ def setup(bot):
         advices[atype.get('advicekey')]=str(atype.get('advicevalue'))
 
 
+@sopel.module.commands('advicelist')
+def advicelist(bot,trigger):
+    bot.say(str(advices))
+
 @sopel.module.commands('advice\s+([A-z]+)\s+([A-z]+)')
 def advice(bot,trigger):
-	adKey = trigger.group(3)
-	targetNick = trigger.group(2)
-
+    adKey = trigger.group(3)
+    targetNick = trigger.group(2)
     if targetNick==bot.nick:
         bot.reply("Das haettest du wohl gerne!")
         return
-
-	if adKey in advices:
-		bot.say(str(trigger.group(2)) + ": " + advices[adKey])
-	else:
+    if adKey in advices:
+        bot.say(str(trigger.group(2)) + ": " + advices[adKey])
+    else:
         bot.say("Ich habe keinen Rat mit dem Schluesselwort " + str(trigger.group(3)) + ". Momentan beherrsche ich " + str(len(advices)) + " Ratschlaege")
 
