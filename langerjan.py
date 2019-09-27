e94cb119e8e5da91a4182252e6eca85689811d10
Timo <situx@users.noreply.github.com>
2019-09-27 16:38:30 +0200

    Update langerjan.py

--- langerjan.py
+++ langerjan.py
@@ -20,3 +20,13 @@ def helloworld(bot, trigger):
 def moin(bot,trigger):
     """Lets the bot reply to a greeting"""
     bot.say('moin')
+    
+@sopel.module.rule('^\s*[Dd]roggelbecher([?!]+)\s*$')
+def droggelbecher(bot,trigger):
+    """Replys Droggelbecher"""
+    if "?" in trigger.group(2):
+        bot.say('Droggelbecher!')
+    elif "!" in trigger.group(2):
+        bot.say('Droggelbecher?')
+    else:
+        bot.say('Droggelbecher')
